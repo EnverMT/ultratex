1a4606c615baf50e8e378b4272b70b194d4eba5d
Enver <enver@git.com>
2023-03-22 21:58:23 +0500

    assets added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,34 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    {{-- Styles --}}
+
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" />
+
+    <!-- Slick -->
+    <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/slick.css') }}" />
+    <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/slick-theme.css') }}" />
+
+    <!-- nouislider -->
+    <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/nouislider.min.css') }}" />
+
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
+
+    <!-- Custom stlylesheet -->
+    <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -53,6 +81,14 @@
             {{ $slot }}
         </main>
     </div>
+
+    <!-- jQuery Plugins -->
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/slick.min.js') }}"></script>
+    <script src="{{ asset('assets/js/nouislider.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.zoom.min.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
 </body>
 
 </html>
