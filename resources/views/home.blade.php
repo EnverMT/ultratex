0429be2ea22fb964db09fb1aea2d46389f316270
EnverMT <anvarmtg@gmail.com>
2023-03-23 17:03:46 +0500

    v1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,36 @@
         </h2>
     </x-slot>
 
-    <div class="mt-8 grid grid-cols-3 gap-10 container">
 
-        @foreach ($categories as $category)
-            <div class="flex flex-col">
-                <div>
-                    <img src="{{ asset('assets/img/shop01.png') }}" alt="">
-                </div>
-                <div class="p-3 m-2">
-                    <h3>{{ __($category->title) }}<br>{{ __('Collection') }}</h3>
-                    <a href="#">{{ __('Shop now') }} <i class="fa fa-arrow-circle-right"></i></a>
-                </div>
+    <!-- SECTION -->
+    <div class="section">
+        <!-- container -->
+        <div class="container">
+            <!-- row -->
+            <div class="row">
+
+                @foreach ($categories as $category)
+                    <!-- shop -->
+                    <div class="col-md-4 col-xs-6">
+                        <div class="shop">
+                            <div class="shop-img">
+                                <img src="{{ asset('assets/img/shop01.png') }}" alt="">
+                            </div>
+                            <div class="shop-body">
+                                <h3>{{ $category->title }}<br>Collection</h3>
+                                <a href="#" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /shop -->
+                @endforeach
+
             </div>
-        @endforeach
-
+            <!-- /row -->
+        </div>
+        <!-- /container -->
     </div>
+    <!-- /SECTION -->
+
+
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,38 +5,64 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
 
     <title>{{ config('app.name', 'Ultratex') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" /> --}}
+
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" />
+
+    <!-- Slick -->
+    <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/slick.css') }}" />
+    <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/slick-theme.css') }}" />
+
+    <!-- nouislider -->
+    <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/nouislider.min.css') }}" />
+
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
+
+    <!-- Custom stlylesheet -->
+    <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 </head>
 
 <body>
-    <div>
-        <!-- Navigation -->
-        @include('layouts.navigation')
+
+    <!-- Navigation -->
+    @include('layouts.navigation')
+
+    <!-- Page Content -->
+    {{-- <main> --}}
+    {{ $slot }}
+    {{-- </main> --}}
 
 
-        <!-- Page Heading -->
-        @if (isset($header))
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endif
-
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
-    </div>
+    <!-- jQuery Plugins -->
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/slick.min.js') }}"></script>
+    <script src="{{ asset('assets/js/nouislider.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.zoom.min.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
 </body>
 
 </html>
