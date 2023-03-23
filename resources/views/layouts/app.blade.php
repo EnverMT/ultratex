f0fcb73666f3feed0124e6e392821cb159195e81
EnverMT <anvarmtg@gmail.com>
2023-03-23 12:20:04 +0500

    cleaned custom css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,50 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    {{-- Styles --}}
-
-    <!-- Google font -->
-    {{-- <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet"> --}}
-
-    <!-- Bootstrap -->
-    <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" />
-
-    <!-- Slick -->
-    <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/slick.css') }}" />
-    <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/slick-theme.css') }}" />
-
-    <!-- nouislider -->
-    <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/nouislider.min.css') }}" />
-
-    <!-- Font Awesome Icon -->
-    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
-
-    <!-- Custom stlylesheet -->
-    <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
+    <title>{{ config('app.name', 'Ultratex') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100">
-        @auth
-            @include('layouts.navigation')
-        @else
-        @endauth
+<body>
+    <div>
+        <!-- Navigation -->
+        @include('layouts.navigation')
+
 
         <!-- Page Heading -->
         @if (isset($header))
@@ -60,35 +32,11 @@
             </header>
         @endif
 
-        @if (Route::has('login'))
-            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
-                @auth
-                @else
-                    <a href="{{ route('login') }}"
-                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
-                        in</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}"
-                            class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
-
         <!-- Page Content -->
         <main>
             {{ $slot }}
         </main>
     </div>
-
-    <!-- jQuery Plugins -->
-    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('assets/js/slick.min.js') }}"></script>
-    <script src="{{ asset('assets/js/nouislider.min.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery.zoom.min.js') }}"></script>
-    <script src="{{ asset('assets/js/main.js') }}"></script>
 </body>
 
 </html>
