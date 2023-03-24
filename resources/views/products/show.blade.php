e6a6e6ff5bd1c2acb912c79185a4bb8f60cef4f9
Enver <enver@git.com>
2023-03-24 23:02:58 +0500

    v1

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -42,68 +42,28 @@
                 <div class="col-md-5">
                     <div class="product-details">
                         <h2 class="product-name">{{ __($product->title) }}</h2>
+
                         <div>
-                            <div class="product-rating">
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star-o"></i>
-                            </div>
-                            <a class="review-link" href="#">10 Review(s) | Add your review</a>
+                            <h3 class="product-price">{{ __($product->price) }} сум</h3>
+                        </div>
+
+
+
+                        <div>
+                            (price/month)*110% тыс сум / месяц ($month=3 месяца)
                         </div>
                         <div>
-                            <h3 class="product-price">${{ __($product->price) }} <del
-                                    class="product-old-price">$990.00</del></h3>
-                            <span class="product-available">In Stock</span>
+                            (price/month)*110% тыс сум / месяц (6 месяца)
                         </div>
+                        <div>
+                            (price/month)*110% тыс сум / месяц (12 месяца)
+                        </div>
+
+                        <br />
+                        <br />
+
                         <p>{{ __($product->description) }}</p>
 
-                        <div class="product-options">
-                            <label>
-                                Size
-                                <select class="input-select">
-                                    <option value="0">X</option>
-                                </select>
-                            </label>
-                            <label>
-                                Color
-                                <select class="input-select">
-                                    <option value="0">Red</option>
-                                </select>
-                            </label>
-                        </div>
-
-                        <div class="add-to-cart">
-                            <div class="qty-label">
-                                Qty
-                                <div class="input-number">
-                                    <input type="number">
-                                    <span class="qty-up">+</span>
-                                    <span class="qty-down">-</span>
-                                </div>
-                            </div>
-                            <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-                        </div>
-
-                        <ul class="product-btns">
-                            <li><a href="#"><i class="fa fa-heart-o"></i> add to wishlist</a></li>
-                            <li><a href="#"><i class="fa fa-exchange"></i> add to compare</a></li>
-                        </ul>
-
-                        <ul class="product-links">
-                            <li>Category:</li>
-                            <li><a href="#">Headphones</a></li>
-                            <li><a href="#">Accessories</a></li>
-                        </ul>
-
-                        <ul class="product-links">
-                            <li>Share:</li>
-                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                            <li><a href="#"><i class="fa fa-envelope"></i></a></li>
-                        </ul>
 
                     </div>
                 </div>
@@ -114,8 +74,8 @@
                     <div id="product-tab">
                         <!-- product tab nav -->
                         <ul class="tab-nav">
-                            <li class="active"><a data-toggle="tab" href="#tab1">Description</a></li>
-                            <li><a data-toggle="tab" href="#tab2">Details</a></li>
+                            <li class="active"><a data-toggle="tab" href="#tab1">Описание</a></li>
+                            <li><a data-toggle="tab" href="#tab2">Условия</a></li>
                         </ul>
                         <!-- /product tab nav -->
 
@@ -154,6 +114,6 @@
     </div>
     <!-- /SECTION -->
 
-    
+
 
 </x-app-layout>
