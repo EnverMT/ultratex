68e59a8f9a7d612a3526c2c3fee0291d0c150284
EnverMT <anvarmtg@gmail.com>
2023-03-24 13:42:35 +0500

    added column isMain to category

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,19 +14,22 @@
             <div class="row">
 
                 @foreach ($categories as $category)
-                    <!-- shop -->
-                    <div class="col-md-4 col-xs-6">
-                        <div class="shop">
-                            <div class="shop-img">
-                                <img src="{{ asset('assets/img/shop01.png') }}" alt="">
-                            </div>
-                            <div class="shop-body">
-                                <h3>{{ $category->title }}<br>Collection</h3>
-                                <a href="#" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+                    @if ($category->isMain)
+                        <!-- shop -->
+                        <div class="col-md-4 col-xs-6">
+                            <div class="shop">
+                                <div class="shop-img">
+                                    <img src="{{ asset('assets/img/shop01.png') }}" alt="">
+                                </div>
+                                <div class="shop-body">
+                                    <h3>{{ $category->title }}<br>Collection</h3>
+                                    <a href="#" class="cta-btn">Shop now <i
+                                            class="fa fa-arrow-circle-right"></i></a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <!-- /shop -->
+                        <!-- /shop -->
+                    @endif
                 @endforeach
 
             </div>
